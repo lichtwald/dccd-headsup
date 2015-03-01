e70a54376bd2bc9e68b6dd48109ca412d765672f
lichtwald <houstin@luftwald.www.hausderkatzen.us>
2015-02-28 19:21:03 -0500

    Stuff

--- dccd_headsup/dccd_headsup.ino
+++ dccd_headsup/dccd_headsup.ino
@@ -1,16 +1,32 @@
 // Subaru WRX STI DCCD gauge display
+// Signal to be measured is a 1 kHz
+// Duty cycle of 100% is full lock
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
  #include <avr/power.h>
 #endif
 
+//Pin 0 is neopixel i2c
 #define PIN 0
+
+//Analog pins are numbered differently than the board shows
+//Analog Pin 1 is Digital Pin 2 is Analog Pin 1
+#define A0 1
+//Analog Pin 2 is Digital Pin 4
+#define A1 2
+//Analog Pin 3 is Digital Pin 3
+
+//Thresold for considering high vs low
+//Typically AVR uses 3V
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(32, PIN);
 
 uint32_t color  = 0xFF0000; // Start red
 uint32_t prevTime;
+
+unsigned long time1;
+unsigned long time2;
 
 void setup() {
 #ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
@@ -31,22 +47,35 @@ void oc(){
   
     for(i=0; i<16; i++) {
       uint32_t c = 0;
-      if(((offset + i) & 7) < 2) c = color; // 4 pixels on...
       pixels.setPixelColor(   i, c); // First eye
       pixels.setPixelColor(31-i, c); // Second eye (flipped)
  
       pixels.show();
-      offset++;
       delay(50);
     }
   
 }
+
+//States:
+//None -- waiting for a high signal
+//High -- High state measured, waiting for low
+//Low -- Low state measured, upon next high calculate
+//Calculate & Write
+
+//We can't assume a common ground, so to find the voltage it requires
+//Subtracting A0 from A1
 
 void loop() {
   //Main loop
   uint8_t  i;
   uint32_t t;
 
-  t = micros();
+  time1 = micros();
+  
+  uint16_t a0read;
+  uint16_t a1read;
+  
+  a0read = analogRead(A0);
+  a1read = analogRead(A1);
   
 }
